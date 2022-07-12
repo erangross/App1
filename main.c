c5f49ceb0e6b1d9c4cf01a5a5b51a3b0f5213dea
Eran Gross <erangross27@gmail.com>
2022-07-12 18:37:29 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num = 0;
+    printf("Please enter a number: \n");
+    scanf("%d",&num);
+    printf("The number you have entered is: %d",num);
+    return 0;
+}
